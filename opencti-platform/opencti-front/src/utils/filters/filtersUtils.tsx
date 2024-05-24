091bc34c12cf293661117ccefcab9be3a7eefea6
Romy Alula <romy.alula@filigran.io>
2024-05-24 15:13:26 +0200

    [frontend] Improve Creation user experience (#5844)

--- opencti-platform/opencti-front/src/utils/filters/filtersUtils.tsx
+++ opencti-platform/opencti-front/src/utils/filters/filtersUtils.tsx
@@ -655,6 +655,11 @@ export const getAvailableOperatorForFilter = (
   return getAvailableOperatorForFilterKey(filterDefinition);
 };
 
+export const useCompleteFilterKeysMap = () => {
+  const { filterKeysSchema } = useAuth().schema;
+  return filterKeysSchema;
+};
+
 export const useBuildFilterKeysMapFromEntityType = (entityTypes = ['Stix-Core-Object']): Map<string, FilterDefinition> => {
   const { filterKeysSchema } = useAuth().schema;
   // 1. case one entity type
@@ -856,4 +861,11 @@ export const convertOperatorToIcon = (operator: string) => {
     default:
       return null;
   }
+};
+
+export const extractAllFilters: (filters: FilterGroup) => Filter[] = (filters: FilterGroup) => {
+  const allFilters: Filter[] = [];
+  allFilters.push(...filters.filters);
+  filters.filterGroups.forEach((filterGroup) => extractAllFilters(filterGroup));
+  return allFilters;
 };
